231f0ad09788a1ed41fafd7edab153062bfe4037
Rohit Nimangre <rohitn1142001@gmail.com>
2022-02-15 20:53:34 +0530

    initial

---
+++ index.js
@@ -0,0 +1,26 @@
+const canvas = document.querySelector('canvas')
+
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Player{
+    constructor(x, y, radius, color){
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.radius = radius;
+    }
+
+    draw(){
+        c.beginPath()
+        c.arc( this.x, this.y, this.radius, 0, Math.PI*2, false);
+        c.fillStyle = this.color;
+        c.fill()
+    }
+}
+
+const player = new Player(100,100,30,'blue');
+console.log(player);
+player.draw();

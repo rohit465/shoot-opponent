4cee092d236ac1fde2621f2b33a2e17613869249
Rohit Nimangre <rohitn1142001@gmail.com>
2022-03-08 15:20:52 +0530

    animations

--- index.js
+++ index.js
@@ -67,12 +67,41 @@ class Enemy {
     }
 }
 
+class Particle {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+        this.alpha = 1;
+    }
+
+    draw() {
+        c.save()
+        c.globalAlpha = this.alpha
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill()
+        c.restore()
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+        this.alpha -= 0.01
+    }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
-const player = new Player(x, y, 30, 'blue');
+const player = new Player(x, y, 10, 'white');
 const projectiles = []
 const enemies = []
+const particles = []
 
 function spawnEnemies() {
     setInterval(() => {
@@ -90,7 +119,7 @@ function spawnEnemies() {
             x = Math.random() + canvas.width
             y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
         }
-        const color = 'green'
+        const color = `hsl(${Math.random() * 360}, 50%, 50%)`
 
         const angle = Math.atan2(
             canvas.height / 2 - y,
@@ -104,44 +133,74 @@ function spawnEnemies() {
         enemies.push(new Enemy(x, y, radius, color, velocity))
     }, 1000)
 }
-
+let animatonID 
 function animate() {
-    requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
+    animationID = requestAnimationFrame(animate)
+    c.fillStyle = 'rgba(0, 0, 0, 0.1)'
+    c.fillRect(0, 0, canvas.width, canvas.height) 
     player.draw();
+    particles.forEach(particle => {
+        particle.update()
+    })
     projectiles.forEach(projectile => {
         projectile.update()
+
+        if(projectile.x + projectile.radius < 0 || projectile.x - projectile.radius > canvas.width || projectile.y + projectile.radius < 0 || projectile.y - projectile.radius > canvas.height){
+            setTimeout(()=>{
+                
+                projectiles.splice(index,1)
+                },0)
+        }
     })
+
     enemies.forEach((enemy,index) => {
         enemy.update()
 
+        const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+
+        if(dist - enemy.radius - player.radius < 1){
+            console.log("END GAME");
+            cancelAnimationFrame(animationID);
+        }
+
         projectiles.forEach((projectile, projectileIndex) =>{
             const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
-            // console.log(dist);
+            // when projectiles touch enemy
             if(dist - enemy.radius - projectile.radius < 1){
-                // console.log('remove from screen');
-                setTimeout(()=>{
-                enemies.splice(index, 1)
-                projectiles.splice(projectileIndex,1)
-                },0)
-                
+                for(let i = 0; i < 8 ; i++){
+                    particles.push(new Particle(projectile.x, projectile.y, 3, enemy.color, {x: Math.random() - 0.5, y: Math.random() - 0.5}))
+                }
+
+                if(enemy.radius -10 > 5) {
+                    gsap.to(enemy, {
+                        radius: enemy.radius - 10
+                    })
+                    setTimeout(()=>{
+                        projectiles.splice(projectileIndex,1)
+                        },0)
+                }else{
+                    setTimeout(()=>{
+                        enemies.splice(index, 1)
+                        projectiles.splice(projectileIndex,1)
+                        },0)
+                }
             }
         })
     })
 }
 
 addEventListener('click', (event) => {
-
+    // console.log(projectiles);
     const angle = Math.atan2(
         event.clientY - canvas.height / 2,
         event.clientX - canvas.width / 2
     )
     const velocity = {
-        x: Math.cos(angle),
-        y: Math.sin(angle)
+        x: Math.cos(angle) * 4,
+        y: Math.sin(angle) * 4
     }
 
-    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity)
+    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'white', velocity)
     )
 })
 
